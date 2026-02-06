0e18e59475bd26c1f3adb59ecf7b57874190bb0f
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-06 13:28:56 +0000

    Add variables example

---
+++ 01-basics/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "Dennis";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
