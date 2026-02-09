4b80e42243baa6dc8fb98f36c31cd947da73bfa4
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-09 17:24:14 +0300

    Implement rectangle area calculation using user input

---
+++ 01-basics/RectangleArea.java
@@ -0,0 +1,25 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        // Calculate the area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("Area is: " + area + "cm");
+
+        scanner.close();
+    }
+}
