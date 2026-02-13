0f737eafb1dc96c2c0292ce2b4d36d33c057b4a3
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-13 03:34:48 +0300

    Demonstrate pre and post increment/ decrement operators in Java

---
+++ 01-basics/IncrementDecrementOperators.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Increment and Decrement Operators
+
+     int x = 1;
+     //x++;
+    // x++;
+     //x++;
+        x--;
+        x--;
+        x--;
+     System.out.println(x);
+    }
+}
