f31801938fc9375afe4327d2a67b8ae51d470a2c
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-13 03:07:33 +0300

    Demonstrate program of +=,  -=,  *=, /=, and %= operators in Java

---
+++ 01-basics/AugmentedAssignmentOperators.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Augmented Assignment
+
+        int x = 10;
+        int y = 2;
+
+        // x +=y;
+       // x -=y;
+        //x *=y;
+        //x /=y;
+        x %=y;
+        
+        System.out.println(x);
+
+    }
+}
