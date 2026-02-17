199bea3e6b0384d8d426166ebc386ffce9d635e8
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-18 00:04:42 +0300

    Add practice program generating random double values

---
+++ 01-basics/RandomDoublePractice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+
+       Random random = new Random();
+       double number;
+
+
+       number = random.nextDouble();
+
+
+        System.out.println(number);
+
+        }
+
+}
