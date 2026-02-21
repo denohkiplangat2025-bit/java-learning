4e7ee7a39b9c920e2e6b5a13e10ce818b07a0032
Infinite_7 <denohkiplangat2025@gmail.com>
2026-02-21 23:52:30 +0300

    Add advanced printf formatting example

---
+++ 01-basics/PrintfAdvacedFormatting.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // printf = is a method used to format output
+        // %[flags][width][.precision][specifier-character]
+        // + = output a plus
+        // , = comma grouping separator
+        // ( = display a minus if negative, space if positive
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+
+        System.out.printf("%.3f\n", price1);
+        System.out.printf("%.3f\n", price2);
+        System.out.printf("%.3f\n", price3);
+
+    }
+}
