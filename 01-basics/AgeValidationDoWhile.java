bb5e7130035fe567cf834d016ccd35cdc94d5da1
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-14 23:06:42 +0300

    Implement input validation using do-while loop

---
+++ 01-basics/AgeValidationDoWhile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // While loop = repeats some code forever while some conditions remains true
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = 0;
+
+        do{
+            System.out.println("Your age CAN'T be negative");
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+        }while (age < 0);
+        System.out.println("Your age is " + age + " years old");
+
+        scanner.close();
+
+    }
+}
