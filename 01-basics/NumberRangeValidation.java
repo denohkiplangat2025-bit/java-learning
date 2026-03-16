49d25f96fffe08e9c2f9b326292585e54ae77728
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-16 23:21:10 +0300

    Implement input validation for numbers between 1 and 10

---
+++ 01-basics/NumberRangeValidation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+
+        while(number < 1 || number > 10){
+            System.out.print("Enter a number between 1 and 10: ");
+            number = scanner.nextInt();
+        }
+        System.out.println("You picked " + number);
+
+        scanner.close();
+
+    }
+}
