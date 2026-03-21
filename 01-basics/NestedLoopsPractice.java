4ecf96c0bb8c821a07c4bee6e8ab35e02220bc26
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-21 13:06:14 +0300

    Add nested loops practice program for pattern generation

---
+++ 01-basics/NestedLoopsPractice.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // nested loop = A loop inside another loop used often with matrices
+        //                 or  Data Structures and Algorithms
+
+        for(int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        }
+    }
