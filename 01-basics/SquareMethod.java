3551159d5b56229aff57dda19e16ddab71ae1eb9
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-24 01:09:00 +0300

    Implement static method returning square of a number

---
+++ 01-basics/SquareMethod.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // method = a block of reusable code that is executed when called
+
+        double result = square(5);
+        System.out.println(result);
+        }
+        static double square(double number){
+        return number*number;
+        }
+    }
