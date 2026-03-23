8b9970573cba612579f5fdf9c1068a912581696d
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-24 01:12:24 +0300

    Implement static method returning cube of a number

---
+++ 01-basics/CubeMethod.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // method = a block of reusable code that is executed when called
+
+        double result = cube(10);
+        System.out.println(result);
+        }
+        static double cube(double number){
+        return number*number*number;
+        }
+    }
