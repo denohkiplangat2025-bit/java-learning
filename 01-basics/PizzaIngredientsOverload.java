42c0e67538945b9a3453fb78c0ed2a235a7e96be
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-25 23:17:18 +0300

    Implement overloaded methods returning pizza ingredients based on type

---
+++ 01-basics/PizzaIngredientsOverload.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // overload methods = methods that share the same name, but different parameters
+        //      signature = name + parameters
+
+        String pizza = bakePizza("flat bread", "mozzarella");
+        System.out.println(pizza);
+        }
+        static String bakePizza(String bread){
+        return bread + "pizza";
+        }
+        static String bakePizza(String bread, String cheese){
+        return cheese + " " +  bread + "pizza";
+        }
+    }
