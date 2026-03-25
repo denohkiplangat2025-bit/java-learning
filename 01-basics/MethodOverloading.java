de3a96ea517beabc086d78df3a795461a17e212c
Infinite_7 <denohkiplangat2025@gmail.com>
2026-03-25 23:02:32 +0300

    Add method overloading example for addition with multiple parameters

---
+++ 01-basics/MethodOverloading.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // overload methods = methods that share the same name, but different parameters
+        //      signature = name + parameters
+
+        System.out.println(add(4,5, 9, 9));
+        }
+        static double add(double a, double b){
+        return a+b;
+        }
+        static double add(double a, double b, double c){
+        return a+b+c;
+        }
+        static double add(double a, double b, double c, double d){
+        return a+b+c+d;
+        }
+    }
