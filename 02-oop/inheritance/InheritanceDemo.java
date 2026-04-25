5487a729db3514fd500a9ecdba321b5a603e667f
Infinite_7 <denohkiplangat2025@gmail.com>
2026-04-25 14:16:26 +0300

    Add main class to demonstrate inheritance hierarchy

---
+++ 02-oop/inheritance/InheritanceDemo.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Inheritance = One class inherits the attributes and methods from another class
+        //                  Child <- Parent
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        //System.out.println(dog.isAlive);
+        //System.out.println(cat.isAlive);
+
+        //dog.eat();
+        //cat.eat();
+        //System.out.println(dog.lives);
+        //System.out.println(cat.lives);
+        //dog.speak();
+        //cat.speak();
+
+        //System.out.println(plant.isAlive);
+        plant.photosynthesize();
+
+    }
+}
