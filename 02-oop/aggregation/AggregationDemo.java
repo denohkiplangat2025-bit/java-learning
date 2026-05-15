a3b973c42470643a39d54d3f5465f5cd17fad231
Infinite_7 <denohkiplangat2025@gmail.com>
2026-05-16 00:49:41 +0300

    Add aggregation demo using Library and Book classes

---
+++ 02-oop/aggregation/AggregationDemo.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Aggregation = Represents a "has-a" relationship between objects.
+        //               One object contains another object as part of its structure , but the contained objects
+        //               can exist independently
+
+        Book book1 = new Book("The Fellow of the Ring", 423);
+        Book book2 = new Book("The Two Towers", 352);
+        Book book3 = new Book("The Return of the King", 416);
+
+        Book[] books = {book1, book2, book3};
+
+        /*for (Book book : books) {
+            System.out.println(book.displayInfo());
+        }*/
+        Library library = new Library("NYC Public Library", 1897, books);
+
+        library.displayInfo();
+        for (Book book : books) {
+            System.out.println(book.displayInfo());
+        }
+
+    }
+}
+
