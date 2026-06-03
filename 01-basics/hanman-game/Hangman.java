ac3754ceb6237ab4e738e87fa7b297583f7b8cbe
Infinite_7 <denohkiplangat2025@gmail.com>
2026-06-04 02:30:55 +0300

    Add console-based Hangman game with ASCII art and word guessing logic.

---
+++ 01-basics/hanman-game/Hangman.java
@@ -0,0 +1,104 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JAVA HANGMAN
+
+        String word = "pizza";
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            wordState.add('_');
+        }
+
+        System.out.println("********************************");
+        System.out.println("Welcome to Java Hangman!");
+        System.out.println("********************************");
+
+       while (wrongGuesses < 6) {
+           System.out.print(getHangmanArt(wrongGuesses));
+           System.out.print("Word: ");
+           for(char c : wordState){
+               System.out.print(c + " ");
+           }
+           System.out.println();
+
+           System.out.print("Guess a letter: ");
+           char guess = scanner.next().toLowerCase().charAt(0);
+
+           if(word.indexOf(guess) >=0){
+               System.out.println("Correct guess!");
+               for(int i= 0; i <word.length(); i++ ){
+                   if(word.charAt(i) == guess){
+                       wordState.set(i, guess);
+
+                   }
+               }
+
+               if(!wordState.contains('_')){
+                   System.out.println(getHangmanArt(wrongGuesses));
+                   System.out.println("YOU WIN");
+                   System.out.println("The word was :" + word);
+                   break;
+               }
+
+           }
+           else{
+               wrongGuesses++;
+               System.out.println("Wrong guess!");
+           }
+       }
+       if(wrongGuesses >= 6){
+           System.out.print(getHangmanArt(wrongGuesses));
+           System.out.println("GAME OVER! ");
+           System.out.println("The word was: " + word);
+       }
+        scanner.close();
+    }
+    static String getHangmanArt(int wrongGuesses) {
+        return switch (wrongGuesses){
+            case 0 -> """
+                    
+                    
+                    
+                    """;
+            case 1 -> """
+                     0
+                    
+                    
+                    """;
+            case 2 -> """
+                     0
+                     |
+                    
+                    """;
+            case 3 -> """
+                     0
+                    /|
+                    
+                    """;
+            case 4 -> """
+                     0
+                    /|\\
+                    
+                    """;
+            case 5 -> """
+                     0
+                    /|\\
+                    /
+                    """;
+            case 6 -> """
+                     0
+                    /|\\
+                    / \\
+                    """;
+            default -> "";
+        };
+    }
+}
