983de80f8d83db12be9fa1e3396e952a801f9d96
Infinite_7 <denohkiplangat2025@gmail.com>
2026-07-05 20:30:01 +0300

    Extend generics demo with Product<T, U> example

--- 01-basics/generics/GenericsExample.java
+++ 01-basics/generics/GenericsExample.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        
         // Generics = A concept where you can write a class, interface, or method that is compatible with different
         //            data types.
         //            <T> type parameter(placeholder that gets replaced with a real type)
