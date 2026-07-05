deb3146437d58ea16386c2ab99a529039c076bfd
Infinite_7 <denohkiplangat2025@gmail.com>
2026-07-05 20:27:06 +0300

    Add Product generic class with multiple type parameters

---
+++ 01-basics/generics/GenericsExample.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Generics = A concept where you can write a class, interface, or method that is compatible with different
+        //            data types.
+        //            <T> type parameter(placeholder that gets replaced with a real type)
+        //            <String> type argument(specifies the type)
+
+        Product<String,Double> product1 = new Product<>("Iphone", 1.0);
+        Product<String,Integer> product2 = new Product<>("Tablet", 700);
+
+        System.out.println(product1.getItem());
+        System.out.println(product1.getPrice());
+        System.out.println(product2.getItem());
+        System.out.println(product2.getPrice());
+    }
+}
