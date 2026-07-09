80f680eb86f700a7a2a355ac4c08a9a5c8d9e07f
Infinite_7 <denohkiplangat2025@gmail.com>
2026-07-09 21:08:20 +0300

    Add multithreading demo using Runnable and Thread

---
+++ 01-basics/multithreading/MultithreadingDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Multithreading = Enables a program to run multiple(Thread = A set of instructions that run independently).
+        //                  Useful for background tasks or time-consuming operations
+
+        Thread thread1 = new Thread(new MyRunnable("PING"));
+        Thread thread2 = new Thread(new MyRunnable("PONG"));
+
+        System.out.println("GAME START! ");
+
+            thread1.start();
+            thread2.start();
+
+            try {
+                thread1.join();
+                thread2.join();
+            }
+
+        catch (InterruptedException e ){
+            System.out.println("Main Thread was interrupted");
+        }
+
+        System.out.println("GAME END!");
+
+    }
+}
